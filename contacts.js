cc27c072c5f85d37a575594459af58f2dfbc9aa5
TatianaChepovaya <taniachepovaya@gmail.com>
2022-01-12 14:53:42 +0200

    ДЗ

---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { randomUUID } = require("crypto");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((item) => Number(item.id) === contactId);
+
+  if (!contact) {
+    return null;
+  }
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => Number(item.id) === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const removeContact = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removeContact;
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: randomUUID(),
+    name,
+    email,
+    phone
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact
+};
